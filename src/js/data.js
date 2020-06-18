8e69cd85984988059e43deb9fc721eb11f8fdd0a
Josh Kaiser <josh.j.kaiser@gmail.com>
2020-06-18 13:38:11 -0700

    Add basic google map

--- src/js/data.js
+++ src/js/data.js
@@ -2,5 +2,26 @@ import '../scss/data.scss';
 import '../scss/nav.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello world');
+    console.log('hello data');
+
+    const MAPAPIKEY = 'AIzaSyBpXm7Z_Rr99Ki3_iB4vHpZlPBicXqrvIk';
+    // Create the script tag, set the appropriate attributes
+    var script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${MAPAPIKEY}&callback=initMap`;
+    script.defer = true;
+    script.async = true;
+
+    // Attach your callback function to the `window` object
+    window.initMap = function() {
+    // JS API is loaded and available
+        let map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 39.50, lng: -98.35 },
+            zoom: 4
+        });
+    };
+
+    
+
+    // Append the 'script' element to 'head'
+    document.head.appendChild(script);
 })
