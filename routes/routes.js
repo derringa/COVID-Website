e094cef2f4335c46b348dcf1f95cee5379055376
Josh Kaiser <josh.j.kaiser@gmail.com>
2020-06-18 18:14:48 -0700

    adds google map state selector

--- routes/routes.js
+++ routes/routes.js
@@ -5,15 +5,14 @@ module.exports = function (app) {
             script: ['/js/home.min.js'],
             title: 'COVID Home'
 	};
-	
         res.render('home', context);
     });
+
     app.get('/data', function (req, res) {
         let context = {
             script: ['/js/data.min.js'],
             title: 'COVID Data'
 	};
-	
         res.render('data', context);
     });
 }
